1c38e5f3b220f287ce69cd7a7880a5e90dc8d54c
javi_bavi <xavierwood3737@gmail.com>
2025-02-09 16:27:24 -0600

    Small changes

--- nvim/lua/plugins/misc/config.lua
+++ nvim/lua/plugins/misc/config.lua
@@ -1,13 +1,36 @@
 local M = {}
 
 M.presence = function()
-    require("presence").setup({
-        neovim_image_text = "testing to see if any of this works",
-    })
+	require("presence").setup({
+		neovim_image_text = "testing to see if any of this works",
+	})
 end
 
-M.substitute = function ()
-    require("rip-substitute").setup({})
+M.substitute = function()
+	require("rip-substitute").setup({})
+end
+
+M.whichkey = function()
+	local wk = require("which-key")
+
+	wk.add({
+        -- Telescope/find mappings
+		{ "<leader>f", group = "Find", desc = "Find" },
+		{ "<leader>ff", desc = "Files", mode = "n" },
+		{ "<leader>fg", desc = "Live Grep", mode = "n" },
+		{ "<leader>fb", desc = "Buffers", mode = "n" },
+		{ "<leader>fh", desc = "Help Tags", mode = "n" },
+		{ "<leader>fn", desc = "Notifications", mode = "n" },
+		{ "<leader>fx", desc = "Trouble", mode = "n" },
+		{ "<leader>fs", desc = "Substitute", mode = "n" },
+		{ "<leader>fa", desc = "Code Action", mode = "n" },
+
+        -- NvimTree 
+		{ "<leader>n", desc = "Toggle NvimTree", mode = "n" },
+
+        -- Aerial
+		{ "<leader>o", desc = "Toggle Aerial", mode = "n" },
+	})
 end
 
 return M
--- nvim/lua/plugins/misc/plugins.lua
+++ nvim/lua/plugins/misc/plugins.lua
@@ -2,25 +2,44 @@ local settings = require("plugins.misc.config")
 local mappings = require("plugins.misc.mappings")
 
 return {
-	{
-		"andweeb/presence.nvim",
-		config = function()
-			settings.presence()
-		end,
-	},
-	{
-		"chrisgrieser/nvim-rip-substitute",
-        config = function ()
+    {
+        "andweeb/presence.nvim",
+        config = function()
+            settings.presence()
+        end,
+    },
+    {
+        "chrisgrieser/nvim-rip-substitute",
+        config = function()
             settings.substitute()
             mappings.substitute()
-        end
-	},
-	{
-		"OXY2DEV/markview.nvim",
-		lazy = false, -- Recommended
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
+        end,
+    },
+    {
+        "OXY2DEV/markview.nvim",
+        lazy = false, -- Recommended
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            preset = "helix",
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+        init = function()
+            settings.whichkey()
+        end,
+    },
 }
