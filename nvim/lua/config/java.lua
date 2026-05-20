d61c3c37e8bbe19f54011e70213e4a8a6a8cbe10
javibavi <xavierwood3737@gmail.com>
2026-05-20 14:20:17 -0500

    Convert nvim/ to a submodule

--- nvim/lua/config/java.lua
+++
@@ -1,8 +0,0 @@
-vim.lsp.config("jdtls", {
-	settings = {
-		java = {
-			-- Custom eclipse.jdt.ls options go here
-		},
-	},
-})
-vim.lsp.enable("jdtls")
--- nvim/lua/plugins/git.lua
+++
@@ -1,3 +0,0 @@
-return {
-	{ "lewis6991/gitsigns.nvim", opts = { current_line_blame = true, signcolumn = true } },
-}
